3f240f690dd7eafed25cb2ea6d138dfe4e8d6dd4
dtpvanduinkerken <dtpvanduinkerken@gmail.com>
2026-06-29 16:46:54 +0200

    update

--- sql/mart/social_media_maand.sql
+++ sql/mart/social_media_maand.sql
@@ -2,22 +2,19 @@ CREATE OR REPLACE TABLE mart.social_media_maand AS
 
 SELECT
 
-  DATE_TRUNC(
-    'month',
-    STRPTIME(
-      datum,
-      '%d-%m-%Y'
-    )
-  ) AS maand,
+    DATE_TRUNC(
+        'month',
+        datum
+    ) AS maand,
 
-  SUM(views) AS views,
+    SUM(views) AS views,
 
-  SUM(
-    likes +
-    comments +
-    shares +
-    saves
-  ) AS engagement
+    SUM(
+        likes +
+        comments +
+        shares +
+        saves
+    ) AS engagement
 
 FROM raw.social_media
 
